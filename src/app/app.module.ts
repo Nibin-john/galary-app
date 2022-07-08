5b8df06146128e2b0e65eac588c91ccd3edd733b
Nibin-john <nibinjohn24@gmail.com>
2022-07-08 13:04:43 +0530

    gallaryapp-completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GalleryAppComponent } from './gallery-app/gallery-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryAppComponent
   ],
   imports: [
     BrowserModule,
